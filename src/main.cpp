2fc2c8773fed80f943046a2ec29f56107b252a2a
Chi-kwan Chan <chanc@arizona.edu>
2022-09-17 21:15:03 -0700

    Start working on the n-body code

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2022 Chi-kwan Chan
+// Copyright (C) 2022 Steward Observatory
+//
+// This file is part of nbody.
+//
+// nbody is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// nbody is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with nbody.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <cstdio>
+#include <CL/sycl.hpp>
+
+int
+main(int argc, char *argv[])
+{
+	(void)printf("nbody: simple SYCL tester\n");
+	return 0;
+}
